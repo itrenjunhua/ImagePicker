151ced94da7c2bfdbb924d68a4a7ea089f06183f
Renj <itrenjunhua@163.com>
2017-12-25 16:54:29 +0800

    创建裁剪遮罩控件，完成矩形框的控件绘制

--- app/src/main/java/com/renj/imageselect/MainActivity.java
+++ app/src/main/java/com/renj/imageselect/MainActivity.java
@@ -1,49 +1,14 @@
 package com.renj.imageselect;
 
 import android.os.Bundle;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.renj.imageselect.weight.PhotoView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ViewPager viewPager;
     private int[] images = {R.mipmap.ic_launcher, R.mipmap.ic_launcher_round, R.mipmap.a1};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        viewPager = (ViewPager) findViewById(R.id.viewpager);
-
-        viewPager.setAdapter(new PagerAdapter() {
-            @Override
-            public int getCount() {
-                return images.length;
-            }
-
-            @Override
-            public boolean isViewFromObject(View view, Object object) {
-                return view == object;
-            }
-
-            @Override
-            public Object instantiateItem(ViewGroup container, int position) {
-                PhotoView photoView = new PhotoView(MainActivity.this);
-                photoView.setImageResource(images[position]);
-                container.addView(photoView);
-                return photoView;
-            }
-
-            @Override
-            public void destroyItem(ViewGroup container, int position, Object object) {
-                container.removeView((View) object);
-            }
-        });
     }
 }
---
+++ app/src/main/java/com/renj/imageselect/weight/CropView.java
@@ -0,0 +1,117 @@
+package com.renj.imageselect.weight;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.RectF;
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.view.View;
+
+import static android.graphics.Canvas.ALL_SAVE_FLAG;
+
+/**
+ * ======================================================================
+ * <p>
+ * 作者：Renj
+ * <p>
+ * 创建时间：2017-12-25   16:13
+ * <p>
+ * 描述：用于指定裁剪形状的控件
+ * <p>
+ * 修订历史：
+ * <p>
+ * ======================================================================
+ */
+public class CropView extends View {
+    // 默认遮罩层颜色
+    private final int DEFAULT_MASK_COLOR = 0xaa000000;
+    // 默认边框颜色
+    private final int DEFAULT_BORDER_COLOR = 0xFFFFFFFF;
+    // 默认边框宽度 1dp
+    private final int DEFAULT_BORDER_WIDTH = 1;
+    // 默认裁剪宽高相等，都为 200dp，默认矩形
+    private final int DEFAULT_SIZE = 200;
+
+    // 用户绘制背景的画笔
+    private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    // 用于绘制边框的画笔
+    private Paint borderPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    // 遮罩层颜色
+    private int maskColor = DEFAULT_MASK_COLOR;
+    // 边框颜色
+    private int borderColor = DEFAULT_BORDER_COLOR;
+    // 边框宽度
+    private int borderWidth = DEFAULT_BORDER_WIDTH;
+    // 裁剪宽度
+    private int cropWidth = dp2Px(DEFAULT_SIZE);
+    // 裁剪高度
+    private int cropHeight = dp2Px(DEFAULT_SIZE);
+    // 裁剪范围
+    private RectF cropRect = new RectF();
+
+    public CropView(Context context) {
+        this(context, null);
+    }
+
+    public CropView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CropView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public CropView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    private void init() {
+        paint.setColor(maskColor);
+        paint.setStyle(Paint.Style.FILL);
+
+        borderPaint.setColor(borderColor);
+        borderPaint.setStrokeWidth(dp2Px(borderWidth));
+        borderPaint.setStyle(Paint.Style.STROKE);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        int width = getWidth();
+        int height = getHeight();
+        RectF rectF = new RectF(0, 0, width, height);
+
+        int saveLayer = canvas.saveLayer(rectF, null, ALL_SAVE_FLAG);
+
+        canvas.drawRect(rectF, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_OUT));
+        int left = (width - cropWidth) / 2;
+        int top = (height - cropHeight) / 2;
+        cropRect = new RectF(left, top, left + cropWidth, top + cropHeight);
+        canvas.drawRect(cropRect, paint);
+        paint.setXfermode(null);
+
+        canvas.drawRect(cropRect,borderPaint);
+
+        canvas.restoreToCount(saveLayer);
+    }
+
+    /**
+     * dp转换成px
+     *
+     * @param dp
+     * @return
+     */
+    private int dp2Px(int dp) {
+        return (int) (getResources().getDisplayMetrics().density * dp + 0.5);
+    }
+}
