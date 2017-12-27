6013366c02de9be89fb1139814faf342703c09fe
Renj <itrenjunhua@163.com>
2017-12-27 18:05:29 +0800

    图片选择界面增加侧滑分目录选择图片功能

--- app/src/main/java/com/renj/imageselect/activity/ImageSelectActivity.java
+++ app/src/main/java/com/renj/imageselect/activity/ImageSelectActivity.java
@@ -2,14 +2,20 @@ package com.renj.imageselect.activity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.ListView;
 
 import com.renj.imageselect.R;
+import com.renj.imageselect.adapter.ImageMenuAdapter;
 import com.renj.imageselect.adapter.ImageSelectAdapter;
 import com.renj.imageselect.model.FolderModel;
 import com.renj.imageselect.model.ImageModel;
@@ -32,18 +38,60 @@ import java.util.List;
  */
 public class ImageSelectActivity extends AppCompatActivity {
     private GridView gvImages;
+    private ListView lvMenu;
+    private DrawerLayout drawerLayout;
     private ImageSelectAdapter imageSelectAdapter;
+    private ImageMenuAdapter imageMenuAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_select);
         gvImages = findViewById(R.id.gv_images);
+        lvMenu = findViewById(R.id.lv_menu);
+        drawerLayout = findViewById(R.id.drawer_layout);
 
         imageSelectAdapter = new ImageSelectAdapter(this);
+        imageMenuAdapter = new ImageMenuAdapter(this);
         gvImages.setAdapter(imageSelectAdapter);
+        lvMenu.setAdapter(imageMenuAdapter);
 
-        requestPermissions();
+        setListener();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions();
+        } else {
+            startLoadImage();
+        }
+    }
+
+    private void setListener() {
+        // 目录条目监听
+        lvMenu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Object itemData = parent.getItemAtPosition(position);
+                if (itemData instanceof FolderModel) {
+                    FolderModel folderModel = (FolderModel) itemData;
+                    imageSelectAdapter.setImageModels(folderModel.folders);
+                }
+                drawerLayout.closeDrawers();
+            }
+        });
+
+    }
+
+    /**
+     * 开始从SD卡中加载图片
+     */
+    private void startLoadImage() {
+        ImageSelectUtil.loadImageForSdCaard(this, new ImageSelectUtil.LoadImageForSdCardFinishListener() {
+            @Override
+            public void finish(List<ImageModel> imageModels, List<FolderModel> folderModels) {
+                imageSelectAdapter.setImageModels(imageModels);
+                imageMenuAdapter.setFolderModels(folderModels);
+            }
+        });
     }
 
     private void requestPermissions() {
@@ -53,17 +101,14 @@ public class ImageSelectActivity extends AppCompatActivity {
 
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                     Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 12);
             } else {
                 ActivityCompat.requestPermissions(this,
                         new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 12);
             }
         } else {
-            ImageSelectUtil.loadImageForSdCaard(this, new ImageSelectUtil.LoadImageForSdCardFinishListener() {
-                @Override
-                public void finish(List<ImageModel> imageModels, List<FolderModel> folderModels) {
-                    imageSelectAdapter.setImageModels(imageModels);
-                }
-            });
+            startLoadImage();
         }
     }
 
@@ -72,12 +117,7 @@ public class ImageSelectActivity extends AppCompatActivity {
                                            String permissions[], int[] grantResults) {
         if (requestCode == 12 && grantResults.length > 0
                 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            ImageSelectUtil.loadImageForSdCaard(this, new ImageSelectUtil.LoadImageForSdCardFinishListener() {
-                @Override
-                public void finish(List<ImageModel> imageModels, List<FolderModel> folderModels) {
-                    imageSelectAdapter.setImageModels(imageModels);
-                }
-            });
+            startLoadImage();
         }
     }
 }
---
+++ app/src/main/java/com/renj/imageselect/adapter/ImageMenuAdapter.java
@@ -0,0 +1,93 @@
+package com.renj.imageselect.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.renj.imageselect.R;
+import com.renj.imageselect.model.FolderModel;
+
+import java.util.List;
+
+/**
+ * ======================================================================
+ * <p>
+ * 作者：Renj
+ * <p>
+ * 创建时间：2017-12-27   17:40
+ * <p>
+ * 描述：图片选择目录Adapter
+ * <p>
+ * 修订历史：
+ * <p>
+ * ======================================================================
+ */
+public class ImageMenuAdapter extends BaseAdapter {
+    private Context context;
+    private List<FolderModel> folderModels;
+
+    public ImageMenuAdapter(Context context) {
+        this.context = context;
+    }
+
+    public ImageMenuAdapter(Context context, List<FolderModel> folderModels) {
+        this.context = context;
+        this.folderModels = folderModels;
+    }
+
+    public void setFolderModels(List<FolderModel> folderModels) {
+        this.folderModels = folderModels;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return folderModels == null ? 0 : folderModels.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return folderModels.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.item_image_menu, null);
+            viewHolder = new ViewHolder(convertView);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        viewHolder.setData(folderModels.get(position));
+        return convertView;
+    }
+
+
+    class ViewHolder {
+        private ImageView imageView;
+        private TextView textView;
+
+        public ViewHolder(View view) {
+            view.setTag(this);
+
+            imageView = view.findViewById(R.id.iv_enum_show);
+            textView = view.findViewById(R.id.tv_enmu_name);
+        }
+
+        public void setData(FolderModel folderModel) {
+            textView.setText(folderModel.name);
+            Glide.with(context).load(folderModel.folders.get(0).path).into(imageView);
+        }
+    }
+}
